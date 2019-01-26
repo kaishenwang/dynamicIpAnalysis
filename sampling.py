3a1936778955588717c0a3a53df754ed8a03f828
kwang40 <kaishenw1995@gmail.com>
2019-01-26 12:25:24 -0600

    add sampling script

---
+++ sampling.py
@@ -0,0 +1,20 @@
+import sys
+import random
+
+targetSampling = 50000
+inputFile = ''
+outputFile = sys.argv[1]
+
+with open(inputFile) as f:
+    domainSet = f.readlines()
+
+ratio = len(domainSet) / targetSampling
+
+outputSize = 0
+with open (outputFile, 'w') as f:
+    for i in range(domainSet):
+        if random.randint(1,ratio) == 1:
+            f.write(domainSet[i] + "\n")
+            outputSize += 1
+            if outputSize == targetSampling:
+                break
