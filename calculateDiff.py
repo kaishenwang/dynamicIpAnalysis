e3606200b506ffd7934df21cb99c2490a2a5ee01
kwang40 <kaishenw1995@gmail.com>
2019-01-27 12:45:24 -0600

    optimize running time

--- calculateDiff.py
+++ calculateDiff.py
@@ -3,11 +3,12 @@ import json
 domainIpDict = {}
 with open('RR0.json') as f:
     rr = f.readlines()
-
+repeatedDomains = {}
 for line in rr:
     data = json.loads(line)
     domain = data["name"]
     if domain in domainIpDict.keys():
+        repeatedDomains[domain] = 1
         continue
     domainIpDict[domain] = {}
     if data["status"] != "NOERROR":
@@ -19,14 +20,18 @@ print ("Unique Domain Count: " + str(len(domainIpDict)))
 differences = []
 for i in range(1, 16):
     differenceCount = 0
-    uniqueDomain = {}
+    for domain in repeatedDomains.keys():
+        repeatedDomains[domain] = 1
     with open('RR'+str(i)+'.json') as f:
         rr = f.readlines()
     for line in rr:
         data = json.loads(line)
         domain = data["name"]
-        if domain in uniqueDomain.keys():
-            continue
+        if domain in repeatedDomains.keys():
+            if repeatedDomains[domain] == 1:
+                repeatedDomains[domain] = 0
+            else:
+                continue
         uniqueDomain[domain] = 1
         if data["status"] != "NOERROR":
             if len(domainIpDict[domain]) > 0:
@@ -34,6 +39,8 @@ for i in range(1, 16):
             continue
         if data["data"]["ipv4_addresses"][0] not in domainIpDict[domain].keys():
             differenceCount += 1
+    print(i)
+    print(differenceCount)
     differences.append(differenceCount)
 
 print (differences)
