5e3b01aa4e44ea8d97a1ba0c9361473a76f3f9d7
kwang40 <kaishenw1995@gmail.com>
2019-01-27 10:05:45 -0600

    deal with repeated domain cases

--- calculateDiff.py
+++ calculateDiff.py
@@ -7,20 +7,27 @@ with open('RR0.json') as f:
 for line in rr:
     data = json.loads(line)
     domain = data["name"]
+    if domain in domainIpDict.keys():
+        continue
     domainIpDict[domain] = {}
     if data["status"] != "NOERROR":
         continue
     for ip in data["data"]["ipv4_addresses"]:
         domainIpDict[domain][ip] = 1
 
+print ("Unique Domain Count: " + str(len(domainIpDict)))
 differences = []
 for i in range(1, 16):
     differenceCount = 0
+    uniqueDomain = {}
     with open('RR'+str(i)+'.json') as f:
         rr = f.readlines()
     for line in rr:
         data = json.loads(line)
         domain = data["name"]
+        if domain in uniqueDomain.keys():
+            continue
+        uniqueDomain[domain] = 1
         if data["status"] != "NOERROR":
             if len(domainIpDict[domain]) > 0:
                 differenceCount += 1
