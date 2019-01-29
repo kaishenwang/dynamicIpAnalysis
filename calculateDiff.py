af51dba666e7f7bdf55bc428ca2e230c947b940a
kwang40 <kaishenw1995@gmail.com>
2019-01-29 10:29:17 -0600

    change analysis script

--- calculateDiff.py
+++ calculateDiff.py
@@ -1,45 +1,45 @@
 import json
+from os import listdir
+from os.path import isfile, join
+
+folderPath = "/home/kwang40/alookupResult/"
+files = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
+
+files.sort()
 
 domainIpDict = {}
-with open('RR0.json') as f:
+validIpCount = 0
+with open(join(folderPath, files[0])) as f:
     rr = f.readlines()
-repeatedDomains = {}
 for line in rr:
     data = json.loads(line)
     domain = data["name"]
-    if domain in domainIpDict.keys():
-        repeatedDomains[domain] = 1
-        continue
     domainIpDict[domain] = {}
     if data["status"] != "NOERROR":
+        validIpCount += 1
         continue
     for ip in data["data"]["ipv4_addresses"]:
         domainIpDict[domain][ip] = 1
+print(str(validIpCount*100.0/len(domainIpDict)) +  "% has valid ip")
 
-print ("Unique Domain Count: " + str(len(domainIpDict)))
-differences = []
-for i in range(1, 16):
-    differenceCount = 0
-    for domain in repeatedDomains.keys():
-        repeatedDomains[domain] = 1
-    with open('RR'+str(i)+'.json') as f:
+for i in range(1, len(files)):
+    with open(join(folderPath, files[i])) as f:
         rr = f.readlines()
+    newIpCount = 0
+    disAppearIpCount = 0
+    ipChangeCount = 0
     for line in rr:
         data = json.loads(line)
         domain = data["name"]
-        if domain in repeatedDomains.keys():
-            if repeatedDomains[domain] == 1:
-                repeatedDomains[domain] = 0
-            else:
-                continue
         if data["status"] != "NOERROR":
             if len(domainIpDict[domain]) > 0:
-                differenceCount += 1
-            continue
-        if data["data"]["ipv4_addresses"][0] not in domainIpDict[domain].keys():
-            differenceCount += 1
-    print(i)
-    print(differenceCount)
-    differences.append(differenceCount)
-
-print (differences)
+                disAppearIpCount += 1
+        elif len(domainIpDict[domain]) == 0:
+            newIpCount += 1
+        elif data["data"]["ipv4_addresses"][0] not in domainIpDict[domain].keys():
+            ipChangeCount += 1
+    message = str(i * 1.0 / 3) + "hours: "
+    message += "new ip " + str(newIpCount*100.0/len(domainIpDict) + ", "
+    message += "disappear ip " + str(disAppearIpCount*100.0/len(domainIpDict) + ", "
+    message += "ip value change " + str(ipChangeCount*100.0/len(domainIpDict)
+    print (message)
