e774186b629f217641fe609010801797db312331
kwang40 <kaishenw1995@gmail.com>
2019-02-01 23:16:36 -0600

    add script to plot cdf for domain change

---
+++ plotDomainCDF.py
@@ -0,0 +1,30 @@
+import sys
+import matplotlib.pyplot as plt
+
+with open(sys.argv[1]) as f:
+    data = f.readlines()
+
+sumChange = []
+newIp = []
+disappearIp = []
+changeIp = []
+for line in data:
+    line = line.rstrip()
+    numberStr = line[line.find('[')+1:-1]
+    numbers = numberStr.split(',')
+    sumChange.append(int(numbers[0]))
+    newIp.append(int(numbers[1]))
+    disappearIp.append(int(numbers[2]))
+    changeIp.append(int(numbers[3]))
+
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.hist(sumChange, bins = 286, range = (0, 285), histtype='step',normed=1, cumulative=True, label='total change')
+ax.hist(newIp, bins = 286, range = (0, 285), histtype='step',normed=1, cumulative=True, label='new Ip ratio')
+ax.hist(disappearIp, bins = 286, range = (0, 285), histtype='step',normed=1, cumulative=True, label='disappear ip ratio')
+ax.hist(changeIp, bins = 286, range = (0, 285), histtype='step',normed=1, cumulative=True, label='change ip ratio')
+ax.grid(True)
+ax.legend(loc='right')
+ax.set_title('CDF')
+ax.set_xlabel('number of time changed(record every 20 mins)')
+ax.set_ylabel('')
+plt.show()
