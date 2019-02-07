bc7a5b954fdbe4876cf0289c09d1b388ca1098a1
kwang40 <kaishenw1995@gmail.com>
2019-02-07 15:10:26 -0600

    modify change ip method and add sliding window method for origin time point

--- calculateDiff.py
+++ calculateDiff.py
@@ -1,7 +1,6 @@
 import json
 from os import listdir
 from os.path import isfile, join
-import math
 
 folderPath = "/home/kwang40/alookupResult/"
 files = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
@@ -9,18 +8,14 @@ files = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
 files.sort()
 
 domainIpDict = {}
-validIpCount = 0
+totalIpCount = 0
 with open(join(folderPath, files[0])) as f:
     rr = f.readlines()
 for line in rr:
+    totalIpCount += 1
     data = json.loads(line)
-    domain = data["name"]
-    domainIpDict[domain] = {}
     if data["status"] == "NOERROR":
-        validIpCount += 1
-        for ip in data["data"]["ipv4_addresses"]:
-            domainIpDict[domain][ip] = 1
-print(str(validIpCount*100.0/len(domainIpDict)) +  "% has valid ip")
+        domainIpDict[data["name"]] = data["data"]["ipv4_addresses"][0]
 
 
 for i in range(1, min(len(files),24*5*3)):
@@ -29,21 +24,27 @@ for i in range(1, min(len(files),24*5*3)):
     newIpCount = 0
     disAppearIpCount = 0
     ipChangeCount = 0
-    validIpCount = len(domainIpDict)
+    validIpCount = totalIpCount
     for line in rr:
         data = json.loads(line)
         domain = data["name"]
         if data["status"] != "NOERROR":
             validIpCount -= 1
-            if len(domainIpDict[domain]) > 0:
+            if domain in domainIpDict.keys():
                 disAppearIpCount += 1
-        elif len(domainIpDict[domain]) == 0:
+        elif domain not in domainIpDict.keys():
             newIpCount += 1
-        elif data["data"]["ipv4_addresses"][0] not in domainIpDict[domain].keys():
-            ipChangeCount += 1
+        else:
+            firstIp = domainIpDict[domain]
+            changeIp = True
+            for ip in data["data"]["ipv4_addresses"]:
+                if firstIp == ip:
+                    changeIp = False
+            if firstIp:
+                ipChangeCount += 1
     message = str(i)+","+str(i/3.0)+","
-    message += str(validIpCount*100.0/len(domainIpDict))+","
-    message += str(newIpCount*100.0/len(domainIpDict))+","
-    message += str(disAppearIpCount*100.0/len(domainIpDict))+","
-    message += str(ipChangeCount*100.0/len(domainIpDict))+","
+    message += str(validIpCount*100.0/totalIpCount)+","
+    message += str(newIpCount*100.0/totalIpCount)+","
+    message += str(disAppearIpCount*100.0/totalIpCount)+","
+    message += str(ipChangeCount*100.0/totalIpCount)+","
     print (message)
---
+++ swCalculateDiff.py
@@ -0,0 +1,68 @@
+import json
+from os import listdir
+from os.path import isfile, join
+from collections import deque
+
+folderPath = "/home/kwang40/alookupResult/"
+deq = deque(maxlen=3)
+
+def updateDeq(fileName):
+    with open(fileName) as f:
+        rr = f.readlines()
+    deq.append(dict())
+    for line in rr:
+        data = json.loads(line)
+        domain = data["name"]
+        if data["status"] == "NOERROR":
+            deq[-1][domain] = {}
+            for ip in data["data"]["ipv4_addresses"]:
+                deq[-1][domain][ip] = 1
+
+files = [f for f in listdir(folderPath) if isfile(join(folderPath, f))]
+
+files.sort()
+
+domainIpDict = {}
+totalIpCount = 0
+with open(join(folderPath, files[0])) as f:
+    rr = f.readlines()
+for line in rr:
+    totalIpCount += 1
+    data = json.loads(line)
+    if data["status"] == "NOERROR":
+        domainIpDict[data["name"]] = data["data"]["ipv4_addresses"][0]
+    else:
+        domainIpDict[data["name"]] = ""
+
+
+updateDeq(join(folderPath, files[1]))
+updateDeq(join(folderPath, files[2]))
+
+for i in range(3, min(len(files),24*5*3)):
+    updateDeq(join(folderPath, files[i]))
+    newIpCount = 0
+    disAppearIpCount = 0
+    ipChangeCount = 0
+    validIpCount = 0
+    for domain,ip in domainIpDict.items():
+        if len(ip) == 0:
+            if len(deq[0][domain]) > 0 and len(deq[1][domain]) > 0 and len(deq[2][domain]) > 0:
+                newIpCount += 1
+        elif len(deq[0][domain]) == 0 and len(deq[1][domain]) == 0 and len(deq[2][domain]) == 0:
+            disAppearIpCount += 1
+        else:
+            firstIp = domainIpDict[domain]
+            changeIp = True
+            for d in deq:
+                if firstIp in d[domain].keys():
+                    changeIp = False
+            if changeIp:
+                ipChangeCount += 1
+        if len(deq[0][domain]) > 0 or len(deq[1][domain]) > 0 or len(deq[2][domain]) > 0:
+            validIpCount += 1
+    message = str(i)+","+str(i/3.0)+","
+    message += str(validIpCount*100.0/len(domainIpDict))+","
+    message += str(newIpCount*100.0/len(domainIpDict))+","
+    message += str(disAppearIpCount*100.0/len(domainIpDict))+","
+    message += str(ipChangeCount*100.0/len(domainIpDict))+","
+    print (message)
