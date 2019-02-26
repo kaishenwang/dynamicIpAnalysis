2cb4db555cbd0216a97afa3f0d0ee9accb74d7d4
Kaishen Wang <kwang40@castleblack.sprai.org>
2019-02-25 22:26:00 -0600

    update absolute path

--- executePP.sh
+++ executePP.sh
@@ -4,4 +4,4 @@ make
 mkdir /home/kwang40/fullData/$(date -u +\%Y-\%m-\%d)
 cd /home/kwang40/fullData/$(date -u +\%Y-\%m-\%d)
 ulimit -n 65536
-~/go/src/github.com/kwang40/pipelineWrapper/pipelineWrapper --url-file=/data1/nsrg/kwang40/2019-01-17-urls.txt --zdns-excutable=/home/kwang40/go/src/github.com/kwang40/zdns/zdns/./zdns --zmap-excutable=/home/kwang40/zmap/src/./zmap | ~/go/src/github.com/kwang40/zgrab/./zgrab --port 80 --http=/ --fullURL=true --output-file=banners.json --http-max-redirects=10 2>zgrabError.txt 1>result.txt
+~/go/src/github.com/kwang40/pipelineWrapper/pipelineWrapper --url-file=/data1/nsrg/kwang40/2019-01-17-urls.txt --zdns-excutable=/home/kwang40/go/src/github.com/kwang40/zdns/zdns/./zdns --zmap-excutable=/home/kwang40/zmap/src/./zmap | /home/kwang40/go/src/github.com/kwang40/zgrab/./zgrab --port 80 --http=/ --fullURL=true --output-file=banners.json --http-max-redirects=10 2>zgrabError.txt 1>result.txt
