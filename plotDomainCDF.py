b958c636291b2a83b4d6232ac53a49c120c795ba
kwang40 <kaishenw1995@gmail.com>
2019-04-03 15:28:40 -0500

    update

--- plotDomainCDF.py
+++ plotDomainCDF.py
@@ -1,5 +1,11 @@
 import sys
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+def fix_hist_step_vertical_line_at_end(ax):
+    axpolygons = [poly for poly in ax.get_children() if isinstance(poly, mpl.patches.Polygon)]
+    for poly in axpolygons:
+        poly.set_xy(poly.get_xy()[:-1])
 
 with open(sys.argv[1]) as f:
     data = f.readlines()
@@ -19,12 +25,12 @@ for line in data:
 
 fig, ax = plt.subplots(figsize=(8, 4))
 ax.hist(sumChange, bins = 360, range = (0, 360), histtype='step',normed=1, cumulative=True, label='total change')
-ax.hist(newIp, bins = 286, range = (0, 285), histtype='step',normed=1, cumulative=True, label='new Ip ratio')
-ax.hist(disappearIp, bins = 286, range = (0, 285), histtype='step',normed=1, cumulative=True, label='disappear ip ratio')
-ax.hist(changeIp, bins = 286, range = (0, 285), histtype='step',normed=1, cumulative=True, label='change ip ratio')
-ax.grid(True)
+ax.hist(newIp, bins = 360, range = (0, 360), histtype='step',normed=1, cumulative=True, label='new Ip ratio')
+ax.hist(disappearIp, bins = 360, range = (0, 360), histtype='step',normed=1, cumulative=True, label='disappear ip ratio')
+ax.hist(changeIp, bins = 360, range = (0, 360), histtype='step',normed=1, cumulative=True, label='change ip ratio')
+fix_hist_step_vertical_line_at_end(ax)
 ax.legend(loc='right')
-ax.set_title('CDF with Sliding Window')
 ax.set_xlabel('number of time changed(record every 20 mins)')
 ax.set_ylabel('Percentage')
-plt.show()
+name = sys.argv[1]
+plt.savefig('/Users/kaishenwang/CT/dynamicIpAnalysis/CDF_'+name[:name.find('.')]+'.png', dpi=500)
--- plotFrequencyCDF.py
+++ plotFrequencyCDF.py
@@ -1,15 +1,22 @@
 import sys
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-with open(sys.argv[1]) as f:
+def fix_hist_step_vertical_line_at_end(ax):
+    axpolygons = [poly for poly in ax.get_children() if isinstance(poly, mpl.patches.Polygon)]
+    for poly in axpolygons:
+        poly.set_xy(poly.get_xy()[:-1])
+
+
+with open('changeTime.txt') as f:
     data = f.readlines()
 data = [float(x.rstrip()) for x in data]
 
 fig, ax = plt.subplots(figsize=(8, 4))
-ax.hist(data, bins = 500, histtype='step',normed=1, cumulative=True, label='total change')
-ax.grid(True)
+ax.hist(data, bins = 500, range = (0, 16),histtype='step',normed=1, cumulative=True, label='total change')
+fix_hist_step_vertical_line_at_end(ax)
 ax.legend(loc='right')
-ax.set_title('CDF')
-ax.set_xlabel('average time to change ip info')
+ax.set_xlabel('average time to change IP info (in minutes)')
 ax.set_ylabel('')
 plt.show()
+plt.savefig('/Users/kaishenwang/CT/dynamicIpAnalysis/average_time_to_change.png', dpi=500)
