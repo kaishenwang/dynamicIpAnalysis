9e003ca72523cec4e0d5925f0cde3a4c314ec86b
kwang40 <kaishenw1995@gmail.com>
2019-04-28 00:00:21 -0500

    update

---
+++ analysisMissing.py
@@ -0,0 +1,40 @@
+import json
+
+openIP = {}
+with open('IPOpen.txt') as f:
+    for line in f:
+        openIP[line.rstrip()] = True
+
+with open('RR.json') as f, open('noOpenPortDomains.txt', 'w') as wf:
+    for line in f:
+        try:
+            data = json.loads(line.rstrip())
+            if data['status'] == 'NOERROR':
+                ip = data['data']['ipv4_addresses'][0]
+                if ip not in openIP:
+                    wf.write(ip + '\n')
+        except:
+            continue
+
+httpCode = {}
+with open('banners.json') as f, open('noHttpDomains.txt', 'w') as wf:
+    for line in f:
+        if line[:4] == 'null':
+            continue
+        data = json.loads(line.rstrip())
+        code = ''
+        if 'response' in data['data']['http']:
+            code = data['data']['http']['response']['status_code']
+        elif 'redirect_response_chain' in in data['data']['http']:
+            code = data['data']['http']['redirect_response_chain']['status_code']
+        else:
+            wf.write(data['domain'] + ',' + data['error'] + '\n')
+        if len(code) > 0:
+            if code not in httpCode:
+                httpCode[code] = 0
+            httpCode[code] += 1
+
+sorted_code = sorted(httpCode.items(), key=lambda x: x[1], reverse=True)
+with open('httpCode.txt', 'w') as wf:
+    for k,v in sorted_code:
+        wf.write(k + ',' + v + '\n')
